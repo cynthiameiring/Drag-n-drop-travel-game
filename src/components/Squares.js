967fb3ee39f63746b60168670ae6743538e21f7c
cynthiameiring <cynthia.meiring@hotmail.com>
2019-12-09 22:39:48 +0100

    Build two blocks with CSS

---
+++ src/components/Squares.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Squares.css";
+
+export default function Squares(props) {
+  //   if (!props.event.tickets) {
+  //     return <div>Loading...</div>;
+  //   }
+
+  return (
+    <div className="letter-container">
+      <div className="letter">A</div>
+      <div className="target"></div>
+    </div>
+  );
+}
