01b249d55d3cc6b81913ad04a60bd9b43d0dd290
cynthiameiring <cynthia.meiring@hotmail.com>
2019-12-12 19:12:07 +0100

    Add sound effects

--- src/components/Adventurer.js
+++ src/components/Adventurer.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import textballoon from "../pictures/textballoon.png";
-import adventurer from "../pictures/adventurer.png";
+import textballoon from "../media/textballoon.png";
+import adventurer from "../media/adventurer.png";
+import UIfx from "uifx";
 import "./Adventurer.css";
+import celebrationAudio from "../media/celebration.wav";
+import failAudio from "../media/fail.wav";
 
 function Adventurer(props) {
+  const celebration = new UIfx(celebrationAudio);
+  const fail = new UIfx(failAudio);
   let string = "Good luck!";
   if (props.guess === "correct") {
     string = "Well done!!";
+    celebration.play();
   } else if (props.guess === "false") {
     string = "Try again!";
+    fail.play();
   }
 
   return (
