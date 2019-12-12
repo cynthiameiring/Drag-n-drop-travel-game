c6be48aed7f7df2ec95c2694bfc72603aec65371
cynthiameiring <cynthia.meiring@hotmail.com>
2019-12-12 20:18:21 +0100

    Add more data and improve styling

--- src/components/Adventurer.js
+++ src/components/Adventurer.js
@@ -6,27 +6,32 @@ import UIfx from "uifx";
 import "./Adventurer.css";
 import celebrationAudio from "../media/celebration.wav";
 import failAudio from "../media/fail.wav";
+import Typist from "react-typist";
 
 function Adventurer(props) {
   const celebration = new UIfx(celebrationAudio);
   const fail = new UIfx(failAudio);
-  let string = "Good luck!";
+  let string = "Drag and drop the letters to guess the word!";
   if (props.guess === "correct") {
-    string = "Well done!!";
+    string = "Well done!! Click 'next' for a new word";
     celebration.play();
   } else if (props.guess === "false") {
-    string = "Try again!";
+    string = "Don't give up, try again!";
     fail.play();
   }
 
   return (
-    <div className="textballoon-container">
+    <div className="bottom-component">
       <div className="container">
         <img className="textballoon" alt="textballoon" src={textballoon}></img>
 
-        <div key={props.randomKey} className="bottom-right">
-          <p className="typewriter-text">{string}</p>
-        </div>
+        <Typist
+          key={props.randomKey}
+          cursor={{ hideWhenDone: true }}
+          className="typewriter-text"
+        >
+          {string}
+        </Typist>
       </div>
       <img className="adventurer" alt="adventurer" src={adventurer}></img>
     </div>
